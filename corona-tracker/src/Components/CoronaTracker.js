8606d2ece4bc47a917e9b879ffbac06a24c64fba
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-02-04 10:36:29 +0600

    Basic updating

---
+++ corona-tracker/src/Components/CoronaTracker.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import styles from "./CoronaTracker.module.css";
+
+export default function CoronaTracker() {
+  const [coronaCases, setCoronaCases] = useState([]);
+  const [updateDate, setUpdateDate] = useState("");
+
+  const getCovidData = async () => {
+    try {
+      const res = await fetch(
+        "https://corona.lmao.ninja/v2/countries/Bangladesh?yesterday&strict&query"
+      );
+      const actualData = await res.json();
+      setCoronaCases(actualData);
+      let date = new Date(actualData.updated);
+      let lastDate = date.toDateString();
+      setUpdateDate(lastDate);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCovidData();
+  }, []);
+
+  return (
+    <div className={styles.mainDiv}>
+      <div>
+        <h2>Corona Cases Tracker for Bangladesh</h2>
+        <p>
+          Country: <span>{coronaCases.country}</span>
+        </p>
+      </div>
+      <div className={styles.mainContent}>
+        <div className={styles.childDiv}>
+          <h3>
+            <span>Total</span> Cases
+          </h3>
+          <p>
+            Total Cases: <span>{coronaCases.cases}</span>
+          </p>
+        </div>
+
+        <div className={styles.childDiv}>
+          <h3>
+            <span>Total</span> Deaths
+          </h3>
+          <p>
+            Total Deaths: <span>{coronaCases.deaths}</span>
+          </p>
+        </div>
+        <div className={styles.childDiv}>
+          <h3>
+            <span>Total</span> Recovery
+          </h3>
+          <p>
+            Total Recovery: <span>{coronaCases.recovered}</span>
+          </p>
+        </div>
+        <div className={styles.childDiv}>
+          <h3>
+            <span>Today</span> Cases
+          </h3>
+          <p>
+            New Cases: <span>{coronaCases.todayCases}</span>
+          </p>
+        </div>
+        <div className={styles.childDiv}>
+          <h3>
+            <span>Today</span> Deaths
+          </h3>
+          <p>
+            New Death: <span>{coronaCases.todayDeaths}</span>
+          </p>
+        </div>
+
+        <div className={styles.childDiv}>
+          <h3>
+            <span>Today</span> Recovery
+          </h3>
+          <p>
+            New Recovery: <span>{coronaCases.todayRecovered}</span>
+          </p>
+        </div>
+
+        {
+          <div className={styles.childDiv}>
+            <h3>
+              <span>Last</span> Updated
+            </h3>
+            <p>{updateDate}</p>
+          </div>
+        }
+      </div>
+    </div>
+  );
+}
---
+++ corona-tracker/src/Components/MainComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CoronaTracker from "./CoronaTracker";
+
+export default function MainComponent() {
+  return (
+    <>
+      <h2>This is Main Component</h2>
+      <CoronaTracker />
+    </>
+  );
+}
